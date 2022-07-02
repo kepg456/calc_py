28846b8b446033b1c115ce19bf85b4876e7b55fa
kepg56 <leonardodavid.chavez@unmsm.edu.pe>
2022-07-01 21:01:30 -0500

    agregue caja de texto superior

--- interfaz.py
+++ interfaz.py
@@ -1,16 +1,25 @@
 from ast import For
+from cgitb import text
 from tkinter import *
 from tkinter.font import BOLD
 
 
 ventana = Tk()
 ventana.title("calculadora")
-ventana.config(width=550,height=550,background="white")
+ventana.config(width=550,height=550,background="#614C47")
+ventana.maxsize(550, 500)
+ventana.minsize(550,500)
 
+caja = Text(width=23, height=2,font=("Courier",30,BOLD))
+caja.place(x=0,y=0)
 
+"""esta es la parte de las teclas de los numeros"""
+
+"""definicion de la funcion de los botones"""
 def click_boton():
     texto = Label(text="boton presionado hijo de puta")
     texto.place(x=0,y=0)
+
 xf=80
 yf=120
 filas=0
@@ -27,5 +36,5 @@ for i in range(10):
             yf=yf+100
         else:
             yf=yf+100
-
+""" aca terminan las teclas de los numeros"""
 ventana.mainloop()

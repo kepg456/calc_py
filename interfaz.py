53453ff0df38fc3650fe97fa8e9b3997f13ea8a6
Kepg <leonardodavid.chavez@unmsm.edu.pe>
2022-07-03 19:08:48 -0500

    intento que los numeros salgan en el text box 01

--- interfaz.py
+++ interfaz.py
@@ -10,22 +10,26 @@ ventana.config(width=550,height=550,background="#614C47")
 ventana.maxsize(550, 500)
 ventana.minsize(550,500)
 
-caja = Text(width=23, height=2,font=("Courier",30,BOLD))
+"""caja = Text(width=23, height=2,font=("Courier",30,BOLD),state="disabled")
 caja.place(x=0,y=0)
+caja.insert(1,"hola")"""
+
+caja1 = Entry(width=23,font=("Courier",30,BOLD),state="normal")
+caja1.place(x=0,y=0)
 
 """esta es la parte de las teclas de los numeros"""
 
 """definicion de la funcion de los botones"""
-def click_boton():
-    texto = Label(text="boton presionado hijo de puta")
-    texto.place(x=0,y=0)
+def click_boton(tecla):
+    caja1.insert(1,tecla)
 
+"""imprimir botones en la ventana"""
 xf=80
 yf=120
 filas=0
 for i in range(10):
     i=i+1
-    boton = Button(text=i-1,height=3,width=6,font=("Courier",13,BOLD),command=click_boton)
+    boton = Button(text=i-1,height=3,width=6,font=("Courier",13,BOLD),command=click_boton(i-1))
     boton.place(x=xf,y=yf)
     xf= xf + 100
     if(xf>300):

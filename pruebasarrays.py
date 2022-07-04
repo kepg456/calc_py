573314ff563af3c01472ec22368019c61edb8cdf
Kepg <leonardodavid.chavez@unmsm.edu.pe>
2022-07-04 15:41:33 -0500

    rra

---
+++ pruebasarrays.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+from setuptools import Command
+
+botones = []
+
+def click(valor):
+    texto = ("presionado ",valor)
+    Label(text=texto).place(x=0,y=0)
+
+for i in range(10):
+    botones.append(Button(text="hola mundo",command = lambda: click(i)))
+
+
+raiz = Tk()
+
+
+botones[3].place(y=40,x=0)
+
+
+raiz.mainloop()
